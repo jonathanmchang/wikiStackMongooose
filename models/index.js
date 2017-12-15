05cd75cc39112922d79b82c98bd003706a55da97
Jonathan Chang <jonathanmchang@gmail.com>
2017-12-15 16:55:54 -0500

    added model validators

--- models/index.js
+++ models/index.js
@@ -6,18 +6,20 @@ db.on('error', console.error.bind(console, 'mongodb connection error:'));
 
 const Schema = mongoose.schema;
 
+const statuses = ['open', 'closed'];
+
 const pageSchema = new Schema({
-  title: String,
-  urlTitle: String,
-  content: String,
+  title: { type: String, required: true },
+  urlTitle: { type: String, required: true },
+  content: { type: String, required: true },
   date: { type: Date, default: Date.now },
-  status: String,
+  status: { type: String, enum: statuses },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 });
 
 const userSchema = new Schema({
-  name: String,
-  email: String,
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
 });
 
 const Page = mongoose.model('Page', pageSchema);
